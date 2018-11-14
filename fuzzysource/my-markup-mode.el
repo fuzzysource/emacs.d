e8c0445054412903992c4d017dfbb615a7aade0e
Dam Tien Long <longdt90@gmail.com>
2018-11-15 00:10:27 +0700

    Fix key binding in markdown mode.

--- fuzzysource/my-markup-mode.el
+++ fuzzysource/my-markup-mode.el
@@ -1,22 +1,19 @@
+(use-package markdown-toc)
+
 (use-package markdown-mode
-  :init
-  (autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-  (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-  
-  (autoload 'gfm-mode "markdown-mode"
-    "Major mode for editing GitHub Flavored Markdown files" t)
+  :commands (markdown-mode gfm-mode)
   :init (setq markdown-command "multimarkdown")
-  :bind
-  ("<M-up>" . markdown-move-up)
-  ("<M-down>" . markdown-move-down)
-  ("<M-left>" . markdown-promote)
-  ("<M-right>" . markdown-demote))
+  :mode (("\\.markdown\\'" . markdown-mode)
+	 ("\\.md\\'" . gfm-mode))
+  :bind (:map markdown-mode-map
+	 ("<M-up>" . markdown-move-up)
+	 ("<M-down>" . markdown-move-down)
+	 ("<M-left>" . markdown-promote)
+	 ("<M-right>" . markdown-demote)
+	 ([f5] . markdown-toc-generate-or-refresh-toc)))
 
 (add-hook 'nxml-mode-hook
 	  (lambda ()
 	    (nxml-child-indent 4) ))
 
-(use-package markdown-toc)
 (provide 'my-markup-mode)
