0dc456b29bc4adc0624595868e7a7e8e0ded20e1
Dam Tien Long <longdt19@viettel.com.vn>
2019-03-12 13:49:37 +0700

    Docker-compose mode.

--- fuzzysource/filetypes/my-docker-mode.el
+++ fuzzysource/filetypes/my-docker-mode.el
@@ -2,4 +2,9 @@
   :init
   (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
 
+(use-package docker-compose-mode
+  :init
+  (add-to-list 'auto-mode-alist '("docker-compose.yaml" . docker-compose-mode))
+  (add-to-list 'auto-mode-alist '("docker-compose.yml" . docker-compose-mode)))
+
 (provide 'my-docker-mode)
